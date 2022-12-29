3f481c2df4e33e5090f620fc4e08f8153208bdb5
Océane Raubitschek <oceane>
2022-12-29 19:34:26 +0100

    started streamlit dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,161 +1,149 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
+import streamlit as st
 import plotly.express as px 
 import plotly.graph_objects as go
-import numpy as np
 
-### Config
-st.set_page_config(
-    page_title="E-commerce",
-    page_icon="💸 ",
-    layout="wide"
-)
-
-DATA_URL = ('https://full-stack-assets.s3.eu-west-3.amazonaws.com/Deployment/e-commerce_data.csv')
-
-### App
-st.title("Build dashboards with Streamlit 🎨")
-
-st.markdown("""
-    Welcome to this awesome `streamlit` dashboard. This library is great to build very fast and
-    intuitive charts and application running on the web. Here is a showcase of what you can do with
-    it. Our data comes from an e-commerce website that simply displays samples of customer sales. Let's check it out.
-
-    Also, if you want to have a real quick overview of what streamlit is all about, feel free to watch the below video 👇
-""")
-
-with st.expander("⏯️ Watch this 15min tutorial"):
-    st.video("https://youtu.be/B2iAodr0fOo")
-
-st.markdown("---")
+from plotly.subplots import make_subplots
 
 
-# Use `st.cache` when loading data is extremly useful
-# because it will cache your data so that your app 
-# won't have to reload it each time you refresh your app
+
+DATA = "get_around_delay_analysis.xlsx"
+
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    data["Date"] = data["Date"].apply(lambda x: pd.to_datetime(",".join(x.split(",")[-2:])))
-    data["currency"] = data["currency"].apply(lambda x: pd.to_numeric(x[1:]))
+def load_data():
+    data = pd.read_excel(DATA)
     return data
 
-st.subheader("Load and showcase data")
-st.markdown("""
 
-    You can use the usual Data Science libraries like `pandas` or `numpy` to load data. 
-    Then simply use [`st.write()`](https://docs.streamlit.io/library/api-reference/write-magic/st.write) to showcase it on your web app. 
-
-""")
 
 data_load_state = st.text('Loading data...')
-data = load_data(1000)
-data_load_state.text("") # change text from "Loading data..." to "" once the the load_data function has run
+data = load_data()
+data_load_state.text("")
 
-## Run the below code if the check is checked ✅
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)    
+#dataset where time_delta_with_previous_rental_in_minutes is not null
 
-## Simple bar chart
-st.subheader("Simple bar chart built directly with Streamlit")
-st.markdown("""
-    You can build simple chart directly with streamlit using:
+df_delta_not_null = data[data['time_delta_with_previous_rental_in_minutes'].notnull()]
+df_delta_not_null.head()
 
-    * [`st.bar_chart`](https://docs.streamlit.io/library/api-reference/charts/st.bar_chart)
-    * [`st.line_chart`](https://docs.streamlit.io/library/api-reference/charts/st.line_chart)
-    * [`st.area_chart`](https://docs.streamlit.io/library/api-reference/charts/st.area_chart)
+#ratio en % des type de location
+ratio_checking_type = data["checkin_type"].value_counts(normalize=True)
 
-    Eventhough it doesn't offer great flexibility, it is still a very simple way to create graphs quickly since 
-    streamlit since these methods accepts a pandas DataFrame or Numpy array as input.
+ratio_state = data["state"].value_counts(normalize=True).round(3)
 
-""")
-currency_per_country = data.set_index("country")["currency"]
-st.bar_chart(currency_per_country)
+# ratio d'annulation selon type de location
+mobile = data[data["checkin_type"] == "mobile"]
+mobile_ratio_state = mobile["state"].value_counts(normalize=True).round(3)
 
-## Bar chart built with plotly 
-st.subheader("Simple bar chart built with Plotly")
-st.markdown("""
-    Now, the best thing about `streamlit` is its compatibility with other libraries. For example, you
-    don't need to actually use built-in charts to create your dashboard, you can use :
-    
-    * [`plotly`](https://docs.streamlit.io/library/api-reference/charts/st.plotly_chart) 
-    * [`matplotlib`](https://docs.streamlit.io/library/api-reference/charts/st.pyplot)
-    * [`bokeh`](https://docs.streamlit.io/library/api-reference/charts/st.bokeh_chart)
-    * ...
+connect = data[data["checkin_type"] == "connect"]
+connect_ratio_state = connect["state"].value_counts(normalize=True).round(3)
 
-    This way, you have all the flexibility you need to build awesome dashboards. 🥰
+# new dataFrame incluant que les retards
+data_retard = data[data["previous_ended_rental_id"] != "NaN"]
 
-""")
-fig = px.histogram(data.sort_values("country"), x="country", y="currency", barmode="group")
-st.plotly_chart(fig, use_container_width=True)
+data_mobile = data_retard[data_retard["checkin_type"] == "mobile"]
+retard_mobil = data_mobile["delay_at_checkout_in_minutes"].mean()
 
+data_connect = data_retard[data_retard["checkin_type"] == "connect"]
+retard_connect = data_connect["delay_at_checkout_in_minutes"].mean()
 
-### Input data 
-st.subheader("Input data")
-st.markdown("""
-    As a final note, you can use data that a user will insert when he/she interacts with your app.
-    This is called *input data*. To collect these, you can do two things:
-    * [Use any of the input widget](https://docs.streamlit.io/library/api-reference/widgets)
-    * [Build a form](https://docs.streamlit.io/library/api-reference/control-flow/st.form)
-
-    Depending on what you need to do, you will prefer one or the other. With a `form`, you actually group
-    input widgets together and send the data right away, which can be useful when you need to filter
-    by several variables.
-
-""")
-
-#### Create two columns
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("**1️⃣ Example of input widget**")
-    country = st.selectbox("Select a country you want to see all time sales", data["country"].sort_values().unique())
-    
-    country_sales = data[data["country"]==country]
-    fig = px.histogram(country_sales, x="Date", y="currency")
-    fig.update_layout(bargap=0.2)
-    st.plotly_chart(fig, use_container_width=True)
-
-with col2:
-    st.markdown("**2️⃣ Example of input form**")
-
-    with st.form("average_sales_per_country"):
-        country = st.selectbox("Select a country you want to see sales", data["country"].sort_values().unique())
-        start_period = st.date_input("Select a start date you want to see your metric")
-        end_period = st.date_input("Select an end date you want to see your metric")
-        submit = st.form_submit_button("submit")
-
-        if submit:
-            avg_period_country_sales = data[(data["country"]==country)]
-            start_period, end_period = pd.to_datetime(start_period), pd.to_datetime(end_period)
-            mask = (avg_period_country_sales["Date"] > start_period) & (avg_period_country_sales["Date"] < end_period)
-            avg_period_country_sales = avg_period_country_sales[mask].mean()
-            st.metric("Average sales during selected period (in $)", np.round(avg_period_country_sales, 2))
-
-
-### Side bar 
-st.sidebar.header("Build dashboards with Streamlit")
-st.sidebar.markdown("""
-    * [Load and showcase data](#load-and-showcase-data)
-    * [Charts directly built with Streamlit](#simple-bar-chart-built-directly-with-streamlit)
-    * [Charts built with Plotly](#simple-bar-chart-built-with-plotly)
-    * [Input Data](#input-data)
-""")
-e = st.sidebar.empty()
-e.write("")
-st.sidebar.write("Made with 💖 by [Jedha](https://jedha.co)")
+# histogramme
+histo_checkout = data[(data["delay_at_checkout_in_minutes"] > -1440) & (data["delay_at_checkout_in_minutes"] < 1440)]
+histo_delta = data[(data["time_delta_with_previous_rental_in_minutes"] > -1440) & (data["time_delta_with_previous_rental_in_minutes"] < 1440)]
 
 
 
-### Footer 
-empty_space, footer = st.columns([1, 2])
 
-with empty_space:
-    st.write("")
 
-with footer:
-    st.markdown("""
-        🍇
-        If you want to learn more, check out [streamlit's documentation](https://docs.streamlit.io/) 📖
-    """)
+st.markdown("<h1 style='text-align: center; color: white;'>Dashbord GetAround</h1>", unsafe_allow_html=True)
+st.markdown("###")
+
+pie = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]],shared_yaxes = True, 
+                    x_title="proportion des type de location et % d'annulation",
+                    subplot_titles=["Distribution de type de location", "proportion des annulation"])
+
+pie.add_trace(go.Pie( 
+    values=ratio_checking_type,
+    labels=['Mobile', 'Connect '],
+    marker_colors = ['#202EBD','#13E7E3'],                      
+    ),
+    row=1, col=1)
+
+pie.add_trace(go.Pie(
+    values=ratio_state,
+    labels=['Non annulée', 'Annulée '],
+    marker_colors = ['#20BD2E','#FF3333'],
+    ),
+    row=1, col=2)
+
+pie.update_layout(
+    width=1200,
+    legend=dict(
+        font=dict(
+            size=16
+        )))
+
+st.plotly_chart(pie)
+
+fig_pie = make_subplots(rows=1, cols=2, specs=[[{"type": "pie"}, {"type": "pie"}]],shared_yaxes = True,
+                        x_title='proportion des annulation par type de location',subplot_titles=["Mobile", "Connect"])
+
+fig_pie.add_trace(go.Pie( 
+    values=mobile_ratio_state,
+    labels=['Non annulée', 'Annulée'],
+    marker_colors = ['#20BD2E','#FF3333'],                      
+    ),
+    row=1, col=1)
+
+fig_pie.add_trace(go.Pie(
+    values=connect_ratio_state,
+    labels=['Non annulée', 'Annulée'],
+    marker_colors = ['#20BD2E','#FF3333'],
+    ),
+    row=1, col=2)
+
+fig_pie.update_layout(
+    width=1200,
+    legend=dict(
+        font=dict(
+            size=16
+        )))
+
+st.plotly_chart(fig_pie)
+
+st.markdown("###")
+
+st.write(f'Le retard moyen pour la version mobil est de {round(retard_mobil)} minutes')
+
+st.write(f'Le retard moyen pour la version connect est de {round(retard_connect)} minutes')
+
+
+fig = px.histogram(histo_delta, x="time_delta_with_previous_rental_in_minutes",
+                      title = 'histogramme sur 24 heures',
+                      color = 'checkin_type',
+                      barmode ='group',
+                      width= 1200,
+                      height = 600
+                      ) 
+fig.update_layout(title_x = 0.5,
+                    legend=dict(
+                        font=dict(
+                    size=16
+                    ))) 
+                            
+st.plotly_chart(fig)
+
+fig = px.histogram(histo_checkout, x="delay_at_checkout_in_minutes",
+                      title = 'histogramme sur 24 heures',
+                      color = 'checkin_type',
+                      barmode ='group',
+                      width= 1200,
+                      height = 600
+                      ) 
+fig.update_layout(title_x = 0.5,
+                legend=dict(
+                    font=dict(
+                size=16
+                ))) 
+      
+st.plotly_chart(fig)
