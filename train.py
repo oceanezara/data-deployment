90ba0c1235954ab34759b452fcb6fc1c4a1328de
Océane Raubitschek <oceane>
2023-01-01 23:17:40 +0100

    train adjusted

--- train.py
+++ train.py
@@ -2,38 +2,21 @@ import pandas as pd
 import numpy as np
 import mlflow
 import time
+from sklearn.model_selection import train_test_split, GridSearchCV 
+from sklearn.preprocessing import  StandardScaler
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler, OneHotEncoder 
-from sklearn.model_selection import train_test_split
-from mlflow.models.signature import infer_signature
 
-
-import os
-import time
-import joblib
-import mlflow
-
-import pandas as pd
-
-from sklearn.model_selection import train_test_split
-from mlflow.models.signature import infer_signature
-
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler, OneHotEncoder 
 
 if __name__ == "__main__":
 
-   # Set your variables for your environment
-    EXPERIMENT_NAME="price_car"
+# Set your variables for your environment
+    EXPERIMENT_NAME="hyperparameter_tuning"
     # Set experiment's info 
     mlflow.set_experiment(EXPERIMENT_NAME)
     # Get our experiment info
     experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
-
+    
     print("training model...")
     
     # Time execution
@@ -42,63 +25,46 @@ if __name__ == "__main__":
     # Call mlflow autolog
     mlflow.sklearn.autolog()
 
-    df = pd.read_csv("./get_around_pricing_project.csv")
-    df = df.iloc[: , 1:]
+    # Import dataset
+    df = pd.read_csv("https://julie-2-next-resources.s3.eu-west-3.amazonaws.com/full-stack-full-time/linear-regression-ft/californian-housing-market-ft/california_housing_market.csv")
 
-    target = "rental_price_per_day"
+    # X, y split 
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
 
-    x = df.drop(target, axis=1)
- 
-    y = df.loc[:,target]
+    # Train / test split 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=0)
-
-    def categorie(x):
-        numeric_features = []
-        categorical_features = []
-        for i,t in x.dtypes.items():
-            if ('float' in str(t)) or ('int' in str(t)) :
-                numeric_features.append(i)
-            else :
-                categorical_features.append(i)
-        return numeric_features, categorical_features
-
-    numeric_features, categorical_features = categorie(x)
-
-    numeric_transformer = Pipeline(steps=[
-            ('scaler', StandardScaler())
-            ])
-    categorical_transformer = Pipeline(steps=[
-            ('encoder', OneHotEncoder(drop='first', handle_unknown='ignore')) 
-            ])
-    preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', numeric_transformer, numeric_features),
-                ("cat", categorical_transformer, categorical_features)
-            ])
-        
-    model = Pipeline(steps=[("Preprocessing", preprocessor),
-                            ("Regressor", LinearRegression())
-                            ]) 
-       
+    # Pipeline 
+    pipe = Pipeline(steps=[
+        ("standard_scaler", StandardScaler()),
+        ("Random_Forest",RandomForestRegressor())
+    ])
 
     with mlflow.start_run(experiment_id = experiment.experiment_id):
 
-        model.fit(x_train, y_train)
+        params_grid = {
+            "Random_Forest__n_estimators": [10, 50], # list(range(10,101, 10)),
+            "Random_Forest__criterion": ["squared_error"],
+            "Random_Forest__max_depth": [5, 20], #list(range(5, 35, 10)) + [None],
+            "Random_Forest__min_samples_split": [2, 20] #list(range(2, 40, 3))
+        }
 
-        predictions = model.predict(x_train)
+        model = GridSearchCV(pipe, params_grid, n_jobs=-1, verbose=3, cv=3, scoring="r2")
+        model.fit(X_train, y_train)
+
+
+        mlflow.log_metric("Train Score", model.score(X_train, y_train))
+        mlflow.log_metric("Test Score", model.score(X_test, y_test))
         
         mlflow.sklearn.log_model(
             sk_model=model,
-            artifact_path="price_car",
-            registered_model_name="price_car_model",
-            signature=infer_signature(x_train, predictions)
+            artifact_path="modeling_housing_market",
+            registered_model_name="random_forest"
         )
-
-    # joblib.dump(model, "model.joblib")
-
-    print("...Done!")
-    print(f"---Total training time: {time.time()-start_time}")
+       
+    print("...Training Done!")
+    print(f"---Total training time: {time.time()-start_time} seconds")
 
 # df = pd.read_csv("./get_around_pricing_project.csv")
 # df = df.iloc[: , 1:]
